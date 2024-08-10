c62f2446dc812a8eb14eedc8e3c9fba015d3bd6c
JarnoPauwels <jarno.pauwels@student.ehb.be>
2024-08-10 16:51:44 +0200

    feat: Added Question And Answer Routing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRouter = require("./routes/users");
+const questionRouter = require("./routes/questions");
+const answerRouter = require("./routes/answers");
 
 const app = express();
 app.use(bodyParser.json());
 app.use("/users", userRouter);
+app.use("/questions", questionRouter);
+app.use("/answers", answerRouter);
 
 module.exports = app;

929a09298b64c5f9f4da818c4a6b2990477001bc
JarnoPauwels <jarno.pauwels@student.ehb.be>
2024-08-12 16:39:13 +0200

    feat(frontend): Updated To Include Theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ThemeToggleProvider from "./utils/ThemeContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeToggleProvider>
+      <App />
+    </ThemeToggleProvider>
+  </StrictMode>
+);

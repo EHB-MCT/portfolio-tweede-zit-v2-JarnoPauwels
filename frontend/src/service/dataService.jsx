5065ee85e8e4ee20505e0ac7f87c60ee1d3e4b57
JarnoPauwels <jarno.pauwels@student.ehb.be>
2024-08-12 15:27:26 +0200

    feat(frontend): Added Q&A Fetch and Post Logic
    Added logic to fetch all questions, to fetch a single question by its ID and a function to fetch all answers for a single question

--- frontend/src/service/dataService.jsx
+++ frontend/src/service/dataService.jsx
@@ -8,10 +8,62 @@ export const fetchUserData = async (userId) => {
   try {
     const apiUrl = `${URL}/users/${userId}`;
     const response = await axios.get(apiUrl);
-    // console.log(response.data);
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error("Error fetching user data:", error);
+    throw error;
+  }
+};
+
+// Fetch all Questions
+export const fetchQuestionData = async () => {
+  try {
+    const apiUrl = `${URL}/questions`;
+    const response = await axios.get(apiUrl);
+    // console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("There was an error fetching the questions!", error);
+  }
+};
+
+// Fetch a single question by ID, including the user that posted the question
+export const fetchQuestionById = async (questionId) => {
+  try {
+    const apiUrl = `${URL}/questions/${questionId}`;
+    const response = await axios.get(apiUrl);
+    const question = response.data;
+
+    // Fetch user data for the question
+    const userData = await fetchUserData(question.user_id);
+    question.user = userData;
+
+    return question;
+  } catch (error) {
+    console.error("Error fetching the question by ID:", error);
+    throw error;
+  }
+};
+
+export const fetchAnswersForQuestion = async (questionId) => {
+  try {
+    const apiUrl = `${URL}/answers/question/${questionId}`;
+    const response = await axios.get(apiUrl);
+    const answers = response.data;
+
+    // console.log(response.data);
+    const answersWithUsers = await Promise.all(
+      answers.map(async (answer) => {
+        const userData = await fetchUserData(answer.user_id);
+        return { ...answer, user: userData };
+      })
+    );
+
+    // console.log(answersWithUsers);
+    return answersWithUsers;
+  } catch (error) {
+    console.error("Error fetching answers:", error);
     throw error;
   }
 };

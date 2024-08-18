d252939c75b3cce65c744550ac2e2a6c8b14f510
JarnoPauwels <jarno.pauwels@student.ehb.be>
2024-08-18 16:16:44 +0200

    feat(frontend-admin): Added User List + Delete Functions
    Added user list component and function to delete a user, a question and an answer.

--- frontend/src/components/admin/AdminDashboard.jsx
+++ frontend/src/components/admin/AdminDashboard.jsx
@@ -4,16 +4,32 @@ import {
   fetchQuestionData,
   fetchAnswersForQuestion,
 } from "../../service/dataService";
-import { fetchAllUsers, addUser, deleteUser } from "../../service/adminService";
+import {
+  fetchAllUsers,
+  addUser,
+  deleteUser,
+  deleteQuestion,
+  deleteAnswer,
+} from "../../service/adminService";
 import { Box, Typography, FormControlLabel, Switch } from "@mui/material";
 import UserForm from "./UserForm";
+import UserList from "./UserList";
+import DeleteConfirmationDialog from "./DeleteConfirmationDialog";
 import FeedbackSnackbar from "./FeedbackSnackbar";
 
 const AdminDashboard = () => {
-  const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState({});
+
+  // Feedback Snackbar
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  // Delete
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+  const [deleteItemId, setDeleteItemId] = useState(null);
 
   const handleSnackbarClose = () => {
     setSnackbarOpen(false);
@@ -22,6 +38,52 @@ const AdminDashboard = () => {
   const handleSnackbarOpen = (message) => {
     setSnackbarMessage(message);
     setSnackbarOpen(true);
+  };
+
+  const handleDeleteDialogOpen = (itemId) => {
+    setDeleteItemId(itemId);
+    setDeleteDialogOpen(true);
+  };
+
+  const handleDeleteDialogClose = () => {
+    setDeleteItemId(null);
+    setDeleteDialogOpen(false);
+  };
+
+  const handleDeleteConfirm = async () => {
+    if (deleteItemId) {
+      try {
+        if (deleteItemId.startsWith("user-")) {
+          const userId = deleteItemId.replace("user-", "");
+          await deleteUser(userId);
+          setUsers(users.filter((user) => user.id !== userId));
+          handleSnackbarOpen("User deleted successfully!");
+        } else if (deleteItemId.startsWith("question-")) {
+          const questionId = deleteItemId.replace("question-", "");
+          await deleteQuestion(questionId);
+          setQuestions(
+            questions.filter((question) => question.id !== questionId)
+          );
+          handleSnackbarOpen("Question deleted successfully!");
+        } else if (deleteItemId.startsWith("answer-")) {
+          const [questionId, answerId] = deleteItemId
+            .replace("answer-", "")
+            .split("-");
+          await deleteAnswer(answerId);
+          const updatedAnswers = {
+            ...answers,
+            [questionId]: answers[questionId].filter(
+              (answer) => answer.id !== answerId
+            ),
+          };
+          setAnswers(updatedAnswers);
+          handleSnackbarOpen("Answer deleted successfully!");
+        }
+      } catch (error) {
+        console.error("Error deleting item:", error);
+      }
+    }
+    handleDeleteDialogClose();
   };
 
   useEffect(() => {
@@ -54,6 +116,12 @@ const AdminDashboard = () => {
         users={users}
         setUsers={setUsers}
         handleSnackbarOpen={handleSnackbarOpen}
+      />
+      <UserList users={users} handleDeleteUser={handleDeleteDialogOpen} />
+      <DeleteConfirmationDialog
+        open={deleteDialogOpen}
+        onClose={handleDeleteDialogClose}
+        onConfirm={handleDeleteConfirm}
       />
       <FeedbackSnackbar
         open={snackbarOpen}
